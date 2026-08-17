7aa704e1833d55cb70b76411b3aa6265c87abceb
Jadav Ruthwik <23211a05b9@bvrit.ac.in>
2026-08-18 05:09:30 +0530

    new project

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    allowedDevOrigins: ["192.168.0.171"],
-  },
+  allowedDevOrigins: ["192.168.0.107"],
 };
 
 export default nextConfig;
